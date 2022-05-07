d2144e2ca3f1827fb50e08ec1596072461836ef5
Sattwik Suman Das <sattwiksuman@users.noreply.github.com>
2022-05-07 21:51:43 +0200

    streamlit app file added

---
+++ streamlit_f1_app.py
@@ -0,0 +1,61 @@
+# %%
+import streamlit as st
+from utils import *
+
+# MAPS
+SEASON_RACE_DICT={
+    2022: ['Imola', 'Australia', 'Saudi Arabia', 'Bahrain']
+}
+
+SESSIONS = {
+    'R': 'Race',
+    'Q': 'Qualifying',
+    'S': 'Sprint',
+    'SQ': 'Sprint Qualifying',
+    'FP1': 'Practice 1',
+    'FP2': 'Practice 2',
+    'FP3': 'Practice 3'
+}
+
+DRIVERS = ['HAM', 'RUS',
+            'SAI','LEC',
+            'VER', 'PER',
+            'RIC', 'NOR',
+            'VET', 'STR',
+            'ALO', 'OCO', 
+            'TSU', 'GAS',
+            'MSC', 'MAG',
+            'BOT', 'ZHO', 
+            'LAT', 'ALB']
+
+DRIVERS_DETAILS = [[44, 'HAM', 'Mercedes'], [63, 'RUS', 'Mercedes'],
+            [55, 'SAI', 'Ferrari'], [16, 'LEC', 'Ferrari'],
+            [33, 'VER', 'Red Bull'], [11, 'PER', 'Red Bull'],
+            [3, 'RIC', 'McLaren'], [4, 'NOR', 'McLaren'],
+            [5, 'VET', 'Aston Martin'], [18, 'STR', 'Aston Martin'],
+            [14, 'ALO', 'Alpine'], [31, 'OCO', 'Alpine'],
+            [22, 'TSU', 'AlphaTauri'], [10, 'GAS', 'AlphaTauri'],
+            [47, 'MSC', 'Haas F1 Team'], [10, 'MAG', 'Haas F1 Team'],
+            [77, 'BOT', 'Alfa Romeo'], [24, 'ZHO', 'Alfa Romeo'],
+            [6, 'LAT', 'Williams'], [53, 'ALB', 'Williams']]
+
+# %%
+# SETTING PAGE CONFIG TO WIDE MODE
+st.set_page_config(layout="wide")
+
+# Streamlit App Layout
+
+# Sidebar
+with st.sidebar:
+    st.header('Inputs')
+    year = st.selectbox(label='Year:', options=SEASON_RACE_DICT.keys(), index=0)
+    grand_prix = st.selectbox(label='Grand Prix', options=SEASON_RACE_DICT[year], index=0)
+    session = st.selectbox(label='Session', options=SESSIONS.values(), index=0)
+    driver_1 = st.selectbox(label='Driver 1', options=DRIVERS, index=0)
+    driver_2 = st.selectbox(label='Driver 2', options=DRIVERS, index=0)
+    # lap_number = st.selectbox(label='Session', options=SESSIONS.values(), index=0)
+
+# Main Body
+st.title('F1 INTERACTIVE')
+
+
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 # %%
 # Import dependencies
+from statistics import mode
 import fastf1 as ff1
 from fastf1 import plotting
 from fastf1 import utils
@@ -11,6 +12,10 @@ import numpy as np
 import pandas as pd
 from requests import Session
 
+import plotly.graph_objects as go
+
+import streamlit as st
+
 
 # Enable the cache by providing the name of the cache folder
 ff1.Cache.enable_cache('cache') 
@@ -21,8 +26,33 @@ def get_telemetry_for_lap_number(laps_driver, lap_number):
     return laps_driver.loc[laps_driver[laps_driver['LapNumber']==lap_number].index.to_list()[0]].get_telemetry().add_distance()
 
 
-# Plot data
+# Load data
+@st.cache
+def load_data(year, grand_prix, session):
+    session_data = ff1.get_session(year, grand_prix, session)
+    session_data.load()
+    return session_data
 
+# Plot Race summary = LapTime vs LapNumber
+def plot_race_summary(year, grand_prix, session, driver_1, driver_2):
+    '''
+    INPUT:
+    year: year of the season; data type = int
+    grand_prix: name of the grand_prix; data type = str
+    session: FP1, FP2, FP3, Q or R, always set to R by default; data type = str
+    driver_1: 3 letter code of driver 1; data type = str
+    driver_2: 3 letter code of driver 2; data type = str
+    lap_number: lap number for lap to be compared; data type = int
+    OUTPUT:
+    This Function prints the chart LapTime against the LapNumber for both drivers from telemetry.
+    Returns the Plotly Figure_Object.
+    '''
+    session_data = load_data(year, grand_prix, session)
+
+    laps_driver_1 = session_data.laps.pick_driver(driver_1)
+    laps_driver_2 = session_data.laps.pick_driver(driver_2)
+
+# Plot driver data comparison
 def plot_driver_comparison_for_lap_number(year, grand_prix, session, driver_1, driver_2, lap_number):
     '''
     INPUT:
@@ -33,11 +63,10 @@ def plot_driver_comparison_for_lap_number(year, grand_prix, session, driver_1, d
     driver_2: 3 letter code of driver 2; data type = str
     lap_number: lap number for lap to be compared; data type = int
     OUTPUT:
-    This Function prints the chart comparison variation of speed of both drivers vs distance from telemetry.
-    Does not return anything.
+    This Function prints the chart comparing variation of speed of both drivers vs distance over one lap from telemetry data.
+    Returns the Plotly Figure_Object.
     '''
-    session_data = ff1.get_session(year, grand_prix, session)
-    session_data.load()
+    session_data = load_data(year, grand_prix, session)
 
     laps_driver_1 = session_data.laps.pick_driver(driver_1)
     laps_driver_2 = session_data.laps.pick_driver(driver_2)
@@ -45,49 +74,79 @@ def plot_driver_comparison_for_lap_number(year, grand_prix, session, driver_1, d
     telemetry_driver_1 = get_telemetry_for_lap_number(laps_driver_1,lap_number)
     telemetry_driver_2 = get_telemetry_for_lap_number(laps_driver_2,lap_number)
 
-    fig, ax = plt.subplots()
-    ax.plot(telemetry_driver_1['Distance'], telemetry_driver_1['Speed'], color='red', label=driver_1)
-    ax.plot(telemetry_driver_2['Distance'], telemetry_driver_2['Speed'], color='blue', label=driver_2)
+    fig = go.Figure()
+    fig = fig.add_trace(go.Scatter(x=telemetry_driver_1['Distance'], 
+                                    y=telemetry_driver_1['Speed'], 
+                                    mode='lines', 
+                                    name=driver_1,
+                                    hovertemplate = 'Speed: %{y}km/h'
+                                    )
+                        )
+    fig = fig.add_trace(go.Scatter(x=telemetry_driver_2['Distance'], 
+                                    y=telemetry_driver_2['Speed'], 
+                                    mode='lines', 
+                                    name=driver_2,
+                                    hovertemplate = 'Speed: %{y}km/h'
+                                    )
+                        )
 
-    ax.set_xlabel('Distance in m')
-    ax.set_ylabel('Speed in km/h')
+    # Edit the layout
+    fig.update_layout(title='Comparing Speed of both drivers through the Lap',
+                    xaxis_title='Distance in m',
+                    yaxis_title='Speed in km/h')
 
-    ax.legend()
-    plt.suptitle(f"Fastest Lap Comparison \n "
-                f"{session_data.event['EventName']} {session_data.event.year} Race")
+    # fig.show()
 
-    plt.show()
+    return fig
+
+
+    
 
 
 # %%
 
 if __name__=='__main__':
-    plot_driver_comparison_for_lap_number(year=2022, grand_prix='Imola', session='R', driver_1='VER', driver_2='LEC', lap_number=5)
+    fig = plot_driver_comparison_for_lap_number(year=2022, grand_prix='Imola', session='R', driver_1='VER', driver_2='LEC', lap_number=5)
+    fig.show()
 
 
 
-    """ 
-    # Getting into the session
-    year, grand_prix, session = 2022, 'Imola', 'R'
 
-    session_data = ff1.get_session(year, grand_prix, session)
-    session_data.load() # This is new with Fastf1 v.2.2
 
-    # This is how it used to be:
-    # laps = quali.load_laps(with_telemetry=True)
+
+    # # Getting into the session
+    # year, grand_prix, session = 2022, 'Imola', 'R'
+
+    # session_data = ff1.get_session(year, grand_prix, session)
+    # session_data.load() # This is new with Fastf1 v.2.2
+
+    # # This is how it used to be:
+    # # laps = quali.load_laps(with_telemetry=True)
     
-    # Driver names as Input
-    driver_1, driver_2 = 'PER', 'VER'
+    # # Driver names as Input
+    # driver_1, driver_2 = 'PER', 'VER'
 
-    # Laps can now be accessed through the .laps object coming from the session
-    laps_driver_1 = session_data.laps.pick_driver(driver_1)
-    laps_driver_2 = session_data.laps.pick_driver(driver_2)
+    # # Laps can now be accessed through the .laps object coming from the session
+    # laps_driver_1 = session_data.laps.pick_driver(driver_1)
+    # laps_driver_2 = session_data.laps.pick_driver(driver_2)
 
-    # Select the fastest lap
-    fastest_driver_1 = laps_driver_1.pick_fastest()
-    fastest_driver_2 = laps_driver_2.pick_fastest()
+    # # Select the fastest lap
+    # fastest_driver_1 = laps_driver_1.pick_fastest()
+    # fastest_driver_2 = laps_driver_2.pick_fastest()
 
-    # Retrieve the telemetry and add the distance column
-    telemetry_driver_1 = fastest_driver_1.get_telemetry().add_distance()
-    telemetry_driver_2 = fastest_driver_2.get_telemetry().add_distance()
-    """
+    # # Retrieve the telemetry and add the distance column
+    # telemetry_driver_1 = fastest_driver_1.get_telemetry().add_distance()
+    # telemetry_driver_2 = fastest_driver_2.get_telemetry().add_distance()
+
+    # fig, ax = plt.subplots()
+    # ax.plot(telemetry_driver_1['Distance'], telemetry_driver_1['Speed'], color='red', label=driver_1)
+    # ax.plot(telemetry_driver_2['Distance'], telemetry_driver_2['Speed'], color='blue', label=driver_2)
+
+    # ax.set_xlabel('Distance in m')
+    # ax.set_ylabel('Speed in km/h')
+
+    # ax.legend()
+    # plt.suptitle(f"Fastest Lap Comparison \n "
+    #             f"{session_data.event['EventName']} {session_data.event.year} Race")
+
+    # plt.show()
